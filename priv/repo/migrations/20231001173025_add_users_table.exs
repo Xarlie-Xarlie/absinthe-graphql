32c70a9978a8788e0f8f83d0290180dae53cbc48
charliecharlie <jeancharles552@gmail.com>
2023-10-01 14:49:20 -0300

    create migrations, schema, postgres container

--- priv/repo/migrations/20231001173025_add_users_table.exs
+++ priv/repo/migrations/20231001173025_add_users_table.exs
@@ -2,6 +2,15 @@ defmodule Twix.Repo.Migrations.AddUsersTable do
   use Ecto.Migration
 
   def change do
+    create table(:users) do
+      add :nickname, :string
+      add :email, :string
+      add :age, :integer
 
+      timestamps()
+    end
+
+    create unique_index(:users, [:nickname])
+    create unique_index(:users, [:email])
   end
 end
