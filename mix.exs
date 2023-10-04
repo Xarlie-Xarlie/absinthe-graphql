e8208fe3ddc9d85deab974eb7293ce1a56bbb8f1
charliecharlie <jeancharles552@gmail.com>
2023-10-03 22:32:25 -0300

    feat: added crudry for traverse changeset errors

--- mix.exs
+++ mix.exs
@@ -42,7 +42,8 @@ defmodule Twix.MixProject do
       {:jason, "~> 1.2"},
       {:plug_cowboy, "~> 2.5"},
       {:absinthe, "~> 1.7"},
-      {:absinthe_plug, "~> 1.5"}
+      {:absinthe_plug, "~> 1.5"},
+      {:crudry, "~> 2.4.0"}
     ]
   end
 
