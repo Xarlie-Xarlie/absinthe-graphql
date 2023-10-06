a0fc7d3fe487a8fa5ffcbb5a07d0b4bef06e890a
charliecharlie <jeancharles552@gmail.com>
2023-10-06 00:40:07 -0300

    feat: added subscription when user follows someone

--- lib/twix_web/resolvers/follow.ex
+++ lib/twix_web/resolvers/follow.ex
@@ -1,5 +1,12 @@
 defmodule TwixWeb.Resolvers.Follow do
   def follow(%{input: %{user_id: user_id, follower_id: follower_id}}, _context) do
-    Twix.add_follower(user_id, follower_id)
+    case Twix.add_follower(user_id, follower_id) do
+      {:ok, result} ->
+        Absinthe.Subscription.publish(TwixWeb.Endpoint, result, new_follow: "new_follow_topic")
+        {:ok, result}
+
+      error ->
+        error
+    end
   end
 end
--- lib/twix_web/schema/types/root.ex
+++ lib/twix_web/schema/types/root.ex
@@ -52,4 +52,12 @@ defmodule TwixWeb.Schema.Types.Root do
       middleware(TranslateErrors)
     end
   end
+
+  object :root_subscription do
+    field :new_follow, :follow_response do
+      config(fn _args, _context ->
+        {:ok, topic: "new_follow_topic"}
+      end)
+    end
+  end
 end
